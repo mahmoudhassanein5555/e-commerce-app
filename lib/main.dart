d639d662b3821158fcb93ae7c0093dd1a7e9da38
Mahmoud_Hassanein <mahmoudmohamedhassanein5@gmail.com>
2026-03-13 01:52:36 +0200

    create e_commerce app

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+
+import 'package:e_commerce_app/core/di/service_locator.dart';
+import 'package:e_commerce_app/feature/app_section/app_section.dart';
+import 'package:e_commerce_app/feature/auth/view/login_screen.dart';
+import 'package:e_commerce_app/feature/auth/view/register_screen.dart';
+import 'package:e_commerce_app/feature/onboarding/onboarding_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  configureDependencies();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "User App",
+      initialRoute: AppSection.routeName,
+      routes: {
+        OnboardingScreen.routeName: (context) => const OnboardingScreen(),
+        LoginScreen.routeName: (context) => const LoginScreen(),
+        RegisterScreen.routeName: (context) => const RegisterScreen(),
+        AppSection.routeName: (context) => const AppSection(),
+      },
+    );
+  }
+}
